a02bbd46f7c6ad3a239f0dd63761ef4b2d1f08b9
Anna Pushkareva <smirnova_spb@mail.ru>
2025-10-08 11:44:09 +0300

    add LoginForm

---
+++ src/forms/login.form.tsx
@@ -0,0 +1,73 @@
+'use client';
+
+import { Button, Form, Input } from '@heroui/react';
+import { FormEvent, useState } from 'react';
+
+interface IProps {
+    onClose: () => void;
+}
+
+const LoginForm = ({ onClose }: IProps) => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+        confirmPassword: '',
+    });
+    const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(formData);
+
+        onClose();
+    };
+
+    return (
+        <Form className='w-full' onSubmit={onSubmit}>
+            <Input
+                isRequired
+                name='email'
+                placeholder='Введите email'
+                type='email'
+                value={formData.email}
+                classNames={{
+                    inputWrapper: 'bg-default-100',
+                    input: 'text-sm focus:outline-none',
+                }}
+                onChange={(e) => {
+                    setFormData({ ...formData, email: e.target.value });
+                }}
+                validate={(value) => {
+                    if (!value) return 'Почта обязательна';
+                    return null;
+                }}
+            />
+            <Input
+                isRequired
+                name='password'
+                placeholder='Введите пароль'
+                type='password'
+                value={formData.password}
+                classNames={{
+                    inputWrapper: 'bg-default-100',
+                    input: 'text-sm focus:outline-none',
+                }}
+                onChange={(e) => {
+                    setFormData({ ...formData, password: e.target.value });
+                }}
+                validate={(value) => {
+                    if (!value) return 'Пароль обязателен';
+                    return null;
+                }}
+            />
+            <div>
+                <Button variant='light' onPress={onClose}>
+                    Отмена
+                </Button>
+                <Button color='primary' type='submit'>
+                    Войти
+                </Button>
+            </div>
+        </Form>
+    );
+};
+
+export default LoginForm;
