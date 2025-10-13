ab5904e0e6e16d0520db6e2d878b1f892773f225
anna <anna.pushkareva@smddev.com>
2025-10-13 16:33:19 +0300

    add signOutFunc, signInWithCredentials methods

---
+++ src/actions/sign-in.ts
@@ -0,0 +1,18 @@
+import { signIn } from '@/auth/auth';
+
+export const signInWithCredentials = async (
+    email: string,
+    password: string
+) => {
+    try {
+        const result = await signIn('credentials', {
+            email,
+            password,
+            redirect: false,
+        });
+
+        return result;
+    } catch (error) {
+        console.error('Ошибка авторизации:', error);
+    }
+};
---
+++ src/actions/sign-out.ts
@@ -0,0 +1,12 @@
+import { signOut } from '@/auth/auth';
+
+export const signOutFunc = async () => {
+    try {
+        const result = await signOut({ redirect: false });
+        console.log('result', result);
+
+        return result;
+    } catch (error) {
+        console.error('Ошибка авторизации:', error);
+    }
+};
