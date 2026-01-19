42922e7235b40a4b7c0e88be806919ed4433262f
anna <anna.pushkareva@smddev.com>
2026-01-19 16:40:19 +0300

    add recipe form

---
+++ src/forms/recipe.form.tsx
@@ -0,0 +1,71 @@
+import { useIngredientStore } from '@/store/ingredient.store';
+import { useRecipeStore } from '@/store/recipe.store';
+import { IRecipe } from '@/types/recipe';
+import { useRouter } from 'next/router';
+import { useState, useTransition } from 'react';
+
+interface RecipeFormProps {
+    initialRecipe?: IRecipe;
+}
+
+interface IIngredientField {
+    id: number;
+    ingredientId: string;
+    quantity: number | null;
+}
+const initialState = {
+    name: '',
+    description: '',
+    imageUrl: '',
+};
+
+const RecipeForm = ({ initialRecipe }: RecipeFormProps) => {
+    const [error, setError] = useState<string | null>(null);
+    const [formData, setFormData] = useState({
+        name: initialRecipe?.name || initialState.name,
+        description: initialRecipe?.description || initialState.description,
+        imageUrl: initialRecipe?.imageUrl || initialState.imageUrl,
+    });
+    const [ingredientFields, setIngredientFields] = useState<
+        IIngredientField[]
+    >(
+        initialRecipe?.ingredients.map((ing, index) => ({
+            id: index,
+            ingredientId: ing.ingredientId,
+            quantity: ing.quantity,
+        })) || [
+            {
+                id: 0,
+                ingredientId: '',
+                quantity: null,
+            },
+        ],
+    );
+
+    const { ingredients } = useIngredientStore();
+    const { addRecipe, updateRecipe } = useRecipeStore();
+    const [isPending, startTransition] = useTransition();
+
+    const router = useRouter();
+
+    const handleAddIngredientField = () => {
+        if (ingredientFields.length >= 10) return;
+
+        setIngredientFields([
+            ...ingredientFields,
+            {
+                id: ingredientFields.length,
+                ingredientId: '',
+                quantity: null,
+            },
+        ]);
+    };
+
+    const handleRemoveIngredientField = (id: number) => {
+        if (ingredientFields.length > 1) {
+            setIngredientFields(
+                ingredientFields.filter((field) => field.id !== id),
+            );
+        }
+    };
+};
